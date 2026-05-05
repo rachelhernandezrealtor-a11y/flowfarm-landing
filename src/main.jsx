e3f250e95b5401b69205b9ce99c918a56c13bb46
rachelhernandezrealtor-a11y <rachelhernandezrealtor@gmail.com>
2026-05-05 13:04:17 -0400

    Unlock Flow Farm dev preview at startup

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import FlowFarmLanding2 from './FlowFarmLanding2'
 
+if (typeof sessionStorage !== 'undefined') {
+  sessionStorage.setItem('ff_access', 'granted')
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <FlowFarmLanding2 />
 )
